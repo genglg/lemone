8d76b3ad2a7339e1ce76296c42d01d18859b831d
genglg <644351063@qq.com>
2018-09-05 15:41:05 +0800

    comm

---
+++ src/main/java/com/example/springrabbitdemo/SpringrabbitdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springrabbitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrabbitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrabbitdemoApplication.class, args);
+	}
+}
